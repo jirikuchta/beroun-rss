f2690985b6fdad1f1d7d0d03f4c05b29d5f02bf2
Jiri Kuchta <jiri.kuchta@email.cz>
2025-11-24 11:59:25 +0100

    pridani exportu do agregovaneno xml

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 import dotazy_obcanu from "./dotazy_obcanu.ts";
 import uredni_deska from "./uredni_deska.ts";
+import poskytnute_informace from "./poskytnute_informace.ts";
 import { getArgs, serialize, getXMLFilePath } from "./common.ts";
 
 const args = getArgs();
 
-const items = (await Promise.all([dotazy_obcanu(args.limit), uredni_deska(args.limit)])).flat();
+const items = (await Promise.all([
+	dotazy_obcanu(args.limit),
+	uredni_deska(args.limit),
+	poskytnute_informace(args.limit)
+])).flat();
+
 items.sort((a, b) => b.date.getTime() - a.date.getTime());
 
 const data = serialize(items, {title: "Beroun", link:"https://mesto-beroun.cz"});
